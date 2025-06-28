c3b94b07b290736d8b1183e68c553abccde2bc72
IaraTassi <iaratassi@hotmail.com>
2025-06-28 09:07:38 -0300

    feat: adiciona exercícios das aulas de fundamentação e variáveis

---
+++ exercicios-basico/javascript/exercicio-fundamentacao/index.js
@@ -0,0 +1,5 @@
+// Questão 1: Crie uma página html simples que ao ser executada no navegador, mostre a mensagem "Olá, meu nome é -----! Este é meu primeiro código JS" no console.
+
+console.log("Olá meu nome é Iara! este é meu primeiro código JS");
+
+// Questão 2: Utilize um arquivo JavaScript externo no exercício.
