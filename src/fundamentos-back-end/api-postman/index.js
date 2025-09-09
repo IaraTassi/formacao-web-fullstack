9c900b4d2a3b5c800a0001a5c42dcd11baa799b6
IaraTassi <iaratassi@hotmail.com>
2025-09-09 10:12:01 -0300

    feat: adiciona exercícios back-end-I

---
+++ src/fundamentos-back-end/api-postman/index.js
@@ -0,0 +1,11 @@
+// 1. Obter informações de um usuário
+//    a. Use o endpoint: https://api.github.com/users/<username>
+//    b. Substitu </username> pelo username do usuário desejado
+
+//! perfil escolhido Linux Torvalds - https://api.github.com/users/torvalds
+
+// 2. Listar os repositórios de um usuário.
+//    a. Use o endpoint: https://api.github.com/users/<username>/repos
+//    b. Substitua <username> pelo username do usuário desejado.
+
+// Faça a mesma requisição para pelo menos 2 usuários e analise o resultado obtido.
